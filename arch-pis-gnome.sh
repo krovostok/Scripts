3cb1a2d42d4f98e230e9be847a680158d76d34ee
krovostok <hardcore@tuta.com>
2024-04-25 03:49:56 +0300

    forgot SUDO blia

--- arch-pis-gnome.sh
+++ arch-pis-gnome.sh
@@ -40,7 +40,8 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf > /dev/null
+sudo echo | sudo tee -a /etc/pacman.conf > /dev/null
+sudo echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf > /dev/null
 sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf > /dev/null
 sudo pacman -Sy
 
