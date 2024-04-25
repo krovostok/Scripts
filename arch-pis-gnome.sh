91ff20cc4dcfeb5ef57aa4d9ee90024ceab14025
krovostok <hardcore@tuta.com>
2024-04-25 03:01:04 +0300

    update arch-pis-gnome.sh 666

--- arch-pis-gnome.sh
+++ arch-pis-gnome.sh
@@ -18,28 +18,21 @@ sed -i '/^options / s/$/ quiet splash nvidia_drm.modeset=1/' "${entry_dir}${entr
 sudo sed -i '/#Color/s/^#//' /etc/pacman.conf
 sudo sed -i '/#ParallelDownloads = 5/s/^#//; s/5/4/' /etc/pacman.conf
 
-# install fonts
-yes | sudo pacman -S --needed noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-ubuntu-font-family ttf-hack
-
-# install nvidia drivers
-yes | sudo pacman -S --needed nvidia nvidia-utils nvidia-settings nvidia-prime
-
-# install pipewire
-yes | sudo pacman -S --needed pipewire pipewire-audio pipewire-alsa pipewire-jack pipewire-pulse
-
-# install bluez
-yes | sudo pacman -S --needed bluez bluez-utils
-sudo systemctl enable bluetooth
-
-# install gnome (and strip it a little)
-yes | sudo pacman -S --needed gnome gnome-tweaks gst-plugins-base gst-plugins-good adw-gtk3
-yes | sudo pacman -R gnome-software
-yes | sudo pacman -Rdd flatpak # (fuck flatpak)
+# install gnome (and strip it a little), fonts, nvidia drivers, pipewire, bluez and fish
+yes | sudo pacman -S \
+gnome gnome-tweaks gst-plugins-base gst-plugins-good \
+noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-ubuntu-font-family ttf-hack \
+nvidia nvidia-utils nvidia-settings nvidia-prime \
+pipewire pipewire-audio pipewire-alsa pipewire-jack pipewire-pulse \
+bluez bluez-utils \
+fish
 sudo systemctl enable gdm
-
-# install fish
-yes | sudo pacman -S --needed fish
+sudo systemctl enable bluetooth
 chsh -s $(which fish)
+
+# fuck flatpak!
+yes | sudo pacman -R gnome-software
+yes | sudo pacman -Rdd flatpak
 
 # enable chaotic-AUR
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
@@ -50,8 +43,8 @@ sudo echo "[chaotic-aur]" >> /etc/pacman.conf
 sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 sudo pacman -Sy
 
-# install basic stuff
-yes | sudo pacman -S --needed micro neofetch menulibre firefox
+# install misc. stuff
+yes | sudo pacman -S --needed micro neofetch menulibre firefox adw-gtk3
 
 # clean up
 sudo ./Scripts/cleaner.sh
