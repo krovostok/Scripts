bad166238610679632d9b0b537ff27aa5d57ecef
krovostok <hardcore@tuta.com>
2024-04-25 03:47:03 +0300

    fix repo add

--- arch-pis-gnome.sh
+++ arch-pis-gnome.sh
@@ -40,8 +40,8 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo echo "[chaotic-aur]" >> /etc/pacman.conf
-sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf > /dev/null
+sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf > /dev/null
 sudo pacman -Sy
 
 # install misc. stuff
